fd34a9dd10febff01ab013014c273291e193ad3c
Abhishek <abhi007sagar@gmail.com>
2024-11-06 14:44:46 +0530

    Added schema and Design

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Appbar(){
+    const session = useSession();
     return <div>
         <div className="flex justify-between">
             <div>
               Musix
             </div>
             <div>
-               <button className="m-2 p-2 bg-blue-400" onClick={()=> signIn()}>SignIn</button>
+                {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={()=> signOut()}>Logout</button>}
+                {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={()=> signIn()}>signIn</button>}
                </div>        
         </div>
     </div>
